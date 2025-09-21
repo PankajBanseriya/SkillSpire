d04bd0299c6d79786b30d581da0cf82025e1e9da
PankajBanseriya <pankajbanseriya@gmail.com>
2025-09-21 15:00:33 +0530

    Fix Connection Issue

--- server/config/database.js
+++ server/config/database.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{
-        console.log("Database Connected Successfully");
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
-    .catch((error)=>{
-        console.log("Database Connection Failled");
-        console.error(error);
-        process.exit(1);
-    })
+        .then(() => {
+            console.log("Database Connected Successfully");
+        })
+        .catch((error) => {
+            console.log("Database Connection Failed");
+            console.error(error);
+            process.exit(1);
+        })
 }
